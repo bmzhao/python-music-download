13f6981f2a97050776a468d3566e1049a02bd150
Brian Zhao <bzspartan@gmail.com>
2017-03-07 14:33:09 -0800

    added progress bar

--- download.py
+++ download.py
@@ -5,6 +5,19 @@ import requests
 from bs4 import BeautifulSoup
 from urllib import parse
 
+from clint.textui import progress
+
+
+def download(url, filename):
+    r = requests.get(url, stream=True)
+    with open(filename, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+
 def get_urls_on_page(url, func):
     result = requests.get(url)
     soup = BeautifulSoup(result.content)
@@ -37,9 +50,7 @@ if __name__ == '__main__':
             seen.add(mp3_url)
             parsed_mp3_url = parse.unquote(mp3_url)
             mp3_name = parsed_mp3_url[parsed_mp3_url.rfind('/'):]
-            mp3 = requests.get(mp3_url).content
-            with open('output/' + mp3_name, 'wb') as f:
-                f.write(mp3)
-            print('downloaded',mp3_name)
+            download(mp3_url, 'output/' + mp3_name)
+            print('downloaded', mp3_name)
 
         seen.add(url)
